e852847bd83a8705f985efe4e6d2f78b691416b3
Yaroslav Krull <krulll@yandex.ru>
2024-09-05 15:02:36 +0500

    Поправил баг, найденный Лёшей, связанный с возможными пропусками записей в БД
    А также немного ускорил работу, уменьшив количество операций доступа к БД
    
    И сделал возможность выхода не только по 'q', но и по 'й':)

--- mydic.py
+++ mydic.py
@@ -6,25 +6,28 @@ db = DB()
 MAX_RATING = 8
 
 number_of_words = db.count()
+copy_db = db.view()
+
 max_rand = number_of_words * MAX_RATING - 1
 
 while True:
 
     while True: 
         random_number = random.randrange(0, max_rand, 1)
-        id = random_number % number_of_words + 1
-        # print(random_number, id, db.get_rating(id), random_number // 8)
-        if db.get_rating(id) > random_number // 8:
+        index = random_number % number_of_words
+        # print(random_number, index, copy_db[index][3], random_number // 8)
+        if copy_db[index][3] > random_number // 8:
             break
 
-    print("Английский       : " + str(db.get_word_en(id)), end=" ")
-    if input() == 'q':
+    print("Английский       : " + str(copy_db[index][1]), end=" ")
+    quit_symbol = input()
+    if quit_symbol == 'q' or quit_symbol == 'й' :
         break
 
-    print("Русский          : " + str(db.get_word_ru(id)))
-    print("Текущий рейтинг  : " + str(db.get_rating(id)))
+    print("Русский          : " + str(copy_db[index][2]))
+    print("Текущий рейтинг  : " + str(copy_db[index][3]))
 
     a = input("Новый рейтинг    : ")
     if a != "":
-        db.refresh_rating(id, int(a))
+        db.refresh_rating(copy_db[index][0], int(a))
     print()
